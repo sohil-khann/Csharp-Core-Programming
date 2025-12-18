0505b93a6e2e7fb87eeae61ac31212800fa7f30b
sohil-khann <khaa40408@gmail.com>
2025-12-18 22:26:00 +0530

    [Sohil khan] Add.Level_01/Program.cs

---
+++ Level_01/Level_01/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Level_01;
+
+public class Program{
+
+public static void main(string[] args){
+
+     //        Problem02_SmallestOfThree.Run();
+      //          Problem03_LargestOfThree.Run();
+        //     Problem04_SumNatural.Run(); 
+        //     Problem05_Voting.Run(); 
+        //    Problem07_SpringSeason.Run();
+        //         Problem08_CountdownWhile.Run();
+        //    Problem06_PosNegZero.Run(); 
+        //    Problem09_CountdownFor.Run(); 
+
+          //   Problem10_SumUntilZero.Run();
+          //   Problem11_SumUntilNonPositive.Run();
+
+           //  Problem12_SumNaturalWhile.Run();
+           //  Problem13_SumNaturalFor.Run(); 
+          //   Problem14_FactorialWhile.Run();
+          //   Problem15_FactorialFor.Run(); 
+          //   Problem16_OddEvenRange.Run(); 
+          //   Problem17_Bonus.Run(); 
+             Problem18_MultiplicationTable.Run();
+            
+
+
+
+  //  Console.WriteLine($" the number is  divisible by 5 : {CheckNumDivByFive.IsDivisibleByFive()}");
+
+Console.ReadKey();
+}
+
+}
