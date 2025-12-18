3bc3aecae76281d34d515d51edf788c7146a6fe4
sohil-khann <khaa40408@gmail.com>
2025-12-18 17:13:42 +0530

    [Sohil khan] Add. solutionof CheckNumDivByFive

---
+++ Level_01/Level_01/CheckNumDivByFive.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Level_01
+{
+    public static class CheckNumDivByFive
+    {
+        public static bool IsDivisibleByFive()
+
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+            return number % 5 == 0;
+        }
+
+    }
+}
