bb9f4419d83b5d555f0db21fc64544c18e6da079
sohil-khann <khaa40408@gmail.com>
2025-12-20 15:56:37 +0530

    [Sohil khan] Add. Level_02/Program.cs

---
+++ Level_02/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+using System.Runtime.Intrinsics.X86;
+public class Program
+{
+    public static void Main()
+    {
+
+
+
+        //FindBonus.runMethod();  
+
+        //FindYoungestFriend.runMethod();
+
+        //FindLargestSecondLargestNum.runMethod();
+        //ReworkOfProgram2.runMethod();
+        //ReverseTheNum.runMethod();
+
+        //BMI.runMethod();
+
+        //BmiUsing2DArr.runMethod();
+        //StudentMarks.runMethod();
+        //StudentMarksUsing2dArray.runMethod();
+
+        FrequencyOfNum.runMethod();
+        Console.ReadKey();
+
+    }
+       
+}
