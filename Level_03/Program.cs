f0d1938184b75ea0b0a7bca51bb9facbb332f79d
sohil-khann <khaa40408@gmail.com>
2025-12-22 17:03:24 +0530

    [Sohil khan] Add. Program.cs

---
+++ Level_03/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//FootBallTeam.Main();
+
+//NumberChecker.Main();
+//NumberChecker02.Main();
+//NumberChecker03.Main();
+//NumberChecker04.Main();
+DisplayCalender.Main();
+//OTP.Main();
+
+
+
+Console.ReadKey();
