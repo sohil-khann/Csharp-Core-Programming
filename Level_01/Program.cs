dabc00a15b459d6d7c0bed5d447eb57e707fb96c
sohil-khann <khaa40408@gmail.com>
2025-12-23 12:16:08 +0530

    [Sohil khan] Update. Program.cs

---
+++ Level_01/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//NumberGuessingGame.Main();
+
+//MaxOfThreeNumbers.Main();
+
+//PrimeNumber.Main();
+//Fibonacci.Main();
+
+Palindrome.Main();
+
+Console.ReadKey();
